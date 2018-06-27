91c12efed6105c6e3b4bf33a9a0736745f59a0cb
Georgy Bunin <georgyb@mprest.com>
2018-06-27 14:45:47 +0300

    remove mx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MxgComponent } from './mxg.component';
-import { MxEditorComponent } from './mx-editor.component';
-import { SvgViewerComponent } from './svg-viewer.component';
 import { SvgLoaderComponent } from './svg-loader.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MxgComponent } from './mxg.component';
-import { MxEditorComponent } from './mx-editor.component';
-import { SvgViewerComponent } from './svg-viewer.component';
 import { SvgLoaderComponent } from './svg-loader.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MxgComponent,
-    MxEditorComponent,
-    SvgViewerComponent,
     SvgLoaderComponent
   ],
   imports: [
